942dcf3e29dadc27fcc401bc7eb5733ed7a48d43
Mo-Moustafa <momoustafa227@gmail.com>
2025-04-16 22:13:38 -0400

    updated code documentation

--- Fairness_Metrics.py
+++ Fairness_Metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 
+# Compute the absolute difference between metric scores for male ('m') and female ('f') users.
 def compute_recGap(metric_by_gender):
 
     rec_gap = abs(metric_by_gender['m'] - metric_by_gender['f'])
@@ -8,15 +9,16 @@ def compute_recGap(metric_by_gender):
 
 
 def compute_compounding_factor(df, metric_by_gender, epsilon=1e-10):
-
+#  Count number of users per gender
     g_counts = df['gender'].value_counts()
     total_users = len(df)
-
+    #  Compute original data distribution (P)
+    # p[0] = proportion of male users, p[1] = proportion of female users
     p = np.array([g_count / total_users for g_count in g_counts])
     print("Original data distribution:")
     print("Males:", p[0])
     print("Females:", p[1])
-
+    #Compute weighted total metric value
     total_metric = p[0] * metric_by_gender['m'] + p[1] * metric_by_gender['f']
     q_male = (p[0] * metric_by_gender['m']) / total_metric
     q_female = (p[1] * metric_by_gender['f']) / total_metric
@@ -24,7 +26,9 @@ def compute_compounding_factor(df, metric_by_gender, epsilon=1e-10):
     print("\nMetric score distribution:")
     print("Males:", q_male)
     print("Females:", q_female)
-
+    # Compute performance-based distribution (Q)
+    # q_male = proportion of performance attributed to males
+    # q_female = proportion of performance attributed to females
     q_male = max(q_male, epsilon)
     q_female = max(q_female, epsilon)
 
